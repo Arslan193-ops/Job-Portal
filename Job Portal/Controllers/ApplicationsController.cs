5918f4c05a53c5d105f7ceb44b87a88c9035b31f
Arslan <arslanmian604@gmail.com>
2025-08-25 19:46:01 +0500

    Created Jobs and applications controllers and viewa

---
+++ Job Portal/Controllers/ApplicationsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Job_Portal.Models;
+
+namespace Job_Portal.Controllers
+{
+    public class ApplicationsController : Controller
+    {
+        private readonly JobPortalContext _context;
+
+        public ApplicationsController(JobPortalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Applications
+        public async Task<IActionResult> Index()
+        {
+            var jobPortalContext = _context.Applications.Include(a => a.Job).Include(a => a.User);
+            return View(await jobPortalContext.ToListAsync());
+        }
+
+        // GET: Applications/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Applications
+                .Include(a => a.Job)
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(m => m.ApplicationId == id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            return View(application);
+        }
+
+        // GET: Applications/Create
+        public IActionResult Create()
+        {
+            ViewData["JobId"] = new SelectList(_context.Jobs, "JobId", "JobId");
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: Applications/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ApplicationId,JobId,UserId,AppliedDate")] Application application)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(application);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JobId"] = new SelectList(_context.Jobs, "JobId", "JobId", application.JobId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", application.UserId);
+            return View(application);
+        }
+
+        // GET: Applications/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Applications.FindAsync(id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+            ViewData["JobId"] = new SelectList(_context.Jobs, "JobId", "JobId", application.JobId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", application.UserId);
+            return View(application);
+        }
+
+        // POST: Applications/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ApplicationId,JobId,UserId,AppliedDate")] Application application)
+        {
+            if (id != application.ApplicationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(application);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApplicationExists(application.ApplicationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JobId"] = new SelectList(_context.Jobs, "JobId", "JobId", application.JobId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", application.UserId);
+            return View(application);
+        }
+
+        // GET: Applications/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Applications
+                .Include(a => a.Job)
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(m => m.ApplicationId == id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            return View(application);
+        }
+
+        // POST: Applications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var application = await _context.Applications.FindAsync(id);
+            if (application != null)
+            {
+                _context.Applications.Remove(application);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ApplicationExists(int id)
+        {
+            return _context.Applications.Any(e => e.ApplicationId == id);
+        }
+    }
+}
